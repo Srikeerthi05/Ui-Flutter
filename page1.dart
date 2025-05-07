d8af71e62a6013e233bf6307c3aa0524a4c756ed
Srikeerthi05 <164448023+Srikeerthi05@users.noreply.github.com>
2025-05-07 11:23:14 +0530

    Create page1.dart

---
+++ page1.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('This is page 1'),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
